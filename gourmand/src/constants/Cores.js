c7cf39a382b9e765bbc72aba3f1912e4b831ab9d
Yohana <yohanaharumi0@gmail.com>
2026-09-17 13:43:41 -0300

    Add files via upload

---
+++ gourmand/src/constants/Cores.js
@@ -0,0 +1,8 @@
+export const Cores = {
+    primaria: '#c1121f',
+    primariaClara: '#fdf5e4',
+    primariaEscura: '#4B0101',
+    secundaria: '#3579a0',
+    secundariaClara: '#AACADE',
+    secundariaEscura: '#003049'
+}
