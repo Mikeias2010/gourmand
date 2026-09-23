3f98ebef227d64d2cace0ff119e3429c21d4911d
Yohana <yohanaharumi0@gmail.com>
2026-09-23 15:02:25 -0300

    Delete gourmand/Cores.js

--- gourmand/Cores.js
+++
@@ -1,9 +0,0 @@
-export const Cores = {
-    primariaClara: '#369A49',
-    primaria: '#0d2818',
-    primariaEscura: '#020202',
-    secundariaClara: '#d8f3dc',
-    secundaria: '#404756',
-    secundariaEscura: '#191C22',
-    terciaria: '#540804',
-}
